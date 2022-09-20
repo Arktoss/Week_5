a630db1e8ca76e2bf2c892eed9c2532d86b3ee83
Arktoss <adamB2106@gmail.com>
2022-09-20 12:47:39 -0400

    penny

---
+++ week_5.2/pennies_from_heaven.py
@@ -0,0 +1,20 @@
+from asyncore import loop
+import os
+import math
+os.system("cls")
+
+pennies = int(input("How much pennies do you have? "))
+while pennies >= 5:
+    if pennies >= 100:
+        print(f"Dollars = {math.floor(pennies / 100)}")
+        pennies -= 100 * math.floor(pennies/100)
+    elif pennies >= 25:
+        print(f"Quarters = {math.floor(pennies / 25)}")
+        pennies -= 25 * math.floor(pennies/25)
+    elif pennies >= 10:
+        print(f"Dime's = {math.floor(pennies / 10)}")
+        pennies -= 10 * math.floor(pennies/10)
+    elif pennies >= 5:
+        print(f"Nickel's = {math.floor(pennies / 5)}")
+        pennies -= 5 * math.floor(pennies/5)
+print(f"pennies = {pennies}")
